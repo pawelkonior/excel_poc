248c5150450816fb39d4c8d01739c4454f9fba91
Paweł Konior <pawel.konior@gmail.com>
2022-10-15 21:53:41 +0200

    init

---
+++ backend_core/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if test "$POSTGRES_DB" = "django"
+then
+  echo "Waiting for postgres"
+
+  while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+    sleep 0.1
+  done
+
+  echo "PostgreSQL started"
+
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+
+exec "$@"
